89f2768fd6ad8d04e527eee76d71f805fe564b50
marleigh <volcana@gmail.com>
2022-02-17 17:22:40 -0800

    if statement to change styles on refactor!

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -1,37 +1,23 @@
-/*
-    set up css classes - on select, change class
-    etc
-
-*/
 
 export default function Answer(props) {
 
-    const defaultStyles = {
-        backgroundColor: props.isSelected ? "green" : "#ccc"
-    }
-
-    const correctSelectedAnswers = {
-        backgroundColor: props.isSelected && props.isCorrectAnswer ? "pink" : "#ccc"
-    }
-
-    const incorrectSelectedAnswers = {
-        backgroundColor: props.isSelected && !props.isCorrectAnswer ? "orange" : "#ccc"
-    }
-
-    const correctUnselectedAnswers = {
-        backgroundColor: props.isCorrect && !props.isSelected ? "blue" : "#ccc"
-    }
-
-
+    //wow this is gross. fix later!
     function checkStyles() {
-        if(props.isSelected) return "green"
-        else if (props.isSelected && props.isCorrectAnswer) return "pink"
-        else if (props.isSelected && !props.isCorrectAnswer) return "orange"
-        else if (props.isCorrect && !props.isSelected) return "blue"
-        else {
-            return "#ccc"
+        if(props.results){
+            if (props.isSelected && props.isCorrectAnswer) return "pink"
+            else if (props.isSelected && !props.isCorrectAnswer) return "orange"
+            else if (props.isCorrect && !props.isSelected) return "blue"
+            else if(props.isSelected) return "green"
+            else {
+                return "#ccc"
+            }
+        } else {
+            if(props.isSelected) return "green"
+            else {
+                return "#ccc"
         }
-    }
+        
+    }}
 
     return (
         <div 
