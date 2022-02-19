a283b0eae5101c498c6707121b8301741cd99f00
marleigh <volcana@gmail.com>
2022-02-18 18:38:40 -0800

    css, changing button styles

--- src/components/Start.js
+++ src/components/Start.js
@@ -3,7 +3,7 @@ export default function Start(props) {
 
     <div className="container--start">
         <h1 className="headline">Quizzical</h1>
-        <p className="start--desc">Description</p>
+        <p className="start--desc">Trivia to test your knowledge of the random! Five multiple choice questions and no time limit.</p>
         <button
             onClick={props.toggle}
             className="btn"
